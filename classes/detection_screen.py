b3dc30cf06997d32b225c47a974f0145200aa9d0
Komputer <Komputer@DESKTOP-QBATHS3>
2020-09-18 23:53:33 +0200

    rozwoj

--- classes/detection_screen.py
+++ classes/detection_screen.py
@@ -6,10 +6,28 @@ class DetectionScreen(Screen):
 	def __init__(self, **kw):
 		super().__init__(**kw)
 
+	def detect(self):
+		if self.ids.haar_checkbox.active:
+			self.haar_detect()
+
 	def haar_detect(self):
 		img = self.ids.face_image.source
 		detection = face_detect_haar(img)
-		if detection is not None:
-			self.ids.face_image.source = detection
 
+		self.ids.result.opacity = 1
+		if detection is not None:
+			self.ids.face_image.source = detection[0]
+			self.ids.result.opacity = 1
+			self.ids.result.text = str(detection[1]) + ' faces detected'
+		else:
+			self.ids.result.text = 'No faces detected'
+
+	def open_file_dialog(self):
+		root = tk.Tk()
+		root.withdraw()
+		file_name = filedialog.askopenfilename(filetypes=[("Image files", ".jpeg .jpg .png .bmp .tiff")])
+
+		self.get_root_window().raise_window()  # set focus on window
+		if file_name != '':
+			self.ids.face_image.source = file_name
 
--- functions/haar_cascade.py
+++ functions/haar_cascade.py
@@ -1,25 +1,35 @@
 import cv2
 import os
+import tkinter as tk
+from tkinter import filedialog
+
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 def face_detect_haar(img_path: str):
     try:
         img_name = os.path.basename(img_path)
         img = cv2.imread(img_path)
+        scale_percent = 50
+        width = int(img.shape[1] * scale_percent / 100)
+        height = int(img.shape[0] * scale_percent / 100)
+        dsize = (width, height)
+        #img = cv2.resize(img, dsize)
+        #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #converts to greyscale
+        #img = cv2.equalizeHist(img)
         faces = face_cascade.detectMultiScale(
             img,
             scaleFactor=1.5,  # specifying how much the image size is reduced at each image scale
             minNeighbors=5,  # specifying how many neighbors each candidate rectangle should have to retain it
-            minSize=(30, 30))
+            minSize=(10, 10))
 
         if len(faces):
             for (x, y, w, h) in faces:
                 img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
             save_path = './detections/'+img_name
             cv2.imwrite(save_path, img)
-        else:
-            print('No faces detected')
 
-        return save_path
+        return save_path, len(faces)
     except:
         print('Image load error or no faces detected')
+
+
