53d45e175394f6c30de2d7ffd77f01fbeb0dc650
pawel <jpawlacz@tlen.pl>
2020-10-09 13:17:35 +0200

    basic face recognizer added

---
+++ functions/face_recognition.py
@@ -0,0 +1,68 @@
+import os
+import cv2
+import numpy as np
+
+
+def create_label_dictionary(dir_path):
+    dict = {}
+    dirs = os.listdir(dir_path)
+    label = 1
+    for dir_name in dirs:
+        dict[label] = dir_name
+        label += 1
+    return dict
+
+def prepare_training_data(dir_path):
+    dirs = os.listdir(dir_path)
+
+    faces = []
+    labels = []
+    label = 1
+    for dir_name in dirs:
+        path = os.path.join(dir_path, dir_name)
+        images = os.listdir(path)
+
+        for image in images:
+            faces.append(cv2.imread(os.path.join(path, image), cv2.IMREAD_GRAYSCALE))
+            labels.append(label)
+        label += 1
+    print(faces, labels)
+
+    return faces, labels
+
+
+def train(faces, labels):
+    print("Faces: ", len(faces))
+    print("Labels: ", len(labels))
+
+    #LBPH
+    face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+    #EigenFaces
+    # face_recognizer = cv2.face.createEigenFaceRecognizer()
+
+    #FisherFaces
+    # face_recognizer = cv2.face.createFisherFaceRecognizer()
+
+    face_recognizer.train(faces, np.array(labels)) #cv2 face recognizer expects numpy array
+    return face_recognizer
+
+def recognize(img, recognizer: cv2.face_LBPHFaceRecognizer):
+    #put face detection function
+    img = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
+    label = recognizer.predict(img) #returns label and confidence (distance) - the longer the distance the less accuracy
+
+    dic = create_label_dictionary('../detections')
+    print(dic[label[0]], label[1])
+    return dic[label[0]], label[1]
+
+
+
+
+if __name__ == '__main__':
+    faces, labels = prepare_training_data('../detections')
+    recognizer = train(faces, labels)
+    recognize('../Images/nieznany.jpg',recognizer)
+    recognize('../Images/dor.jpg', recognizer)
+    recognize('../Images/nieznany2.jpg', recognizer)
+    recognize('../Images/add.png', recognizer)
