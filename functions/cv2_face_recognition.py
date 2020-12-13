307129fdcb92ce6866e939892800f4278dd54adb
pawel <jpawlacz@tlen.pl>
2020-12-13 11:34:55 +0100

    deleted unnecessary comments

--- functions/cv2_face_recognition.py
+++ functions/cv2_face_recognition.py
@@ -157,7 +157,7 @@ if __name__ == '__main__':
     import numpy as np
     from matplotlib import pyplot as plt
 
-    img = cv2.imread('../Images/pawel_19102020_135138.jpg', 0)
+    #img = cv2.imread('img.jpg', 0)
     sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=5)
     sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=5)
 
